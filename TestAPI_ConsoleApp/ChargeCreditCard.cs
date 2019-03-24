2df9c391c7d313626db1c7c87b904805d95352db
mobildeva <mobildeva@gmail.com>
2019-03-24 11:17:59 -0400

    Frist deploy of this project

---
+++ TestAPI_ConsoleApp/ChargeCreditCard.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers.Bases;
+
+namespace TestAPI_ConsoleApp
+{
+    public static class ChargeCreditCard
+    {
+        public static void Run(string ApiLoginID, string ApiTransactionKey)
+        {
+            Console.WriteLine("Charge Credit Card Sample");
+
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+
+            // define the merchant information (authentication / transaction id)
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
+            {
+                name = ApiLoginID,
+                ItemElementName = ItemChoiceType.transactionKey,
+                Item = ApiTransactionKey,
+            };
+
+            var creditCard = new creditCardType
+            {
+                cardNumber = "4111111111111111",
+                expirationDate = "0719"
+            };
+
+            //standard api call to retrieve response
+            var paymentType = new paymentType { Item = creditCard };
+
+            var transactionRequest = new transactionRequestType
+            {
+                transactionType = transactionTypeEnum.authCaptureTransaction.ToString(),   // charge the card
+                amount = 133.45m,
+                payment = paymentType
+            };
+
+            var request = new createTransactionRequest { transactionRequest = transactionRequest };
+
+            // instantiate the contoller that will call the service
+            var controller = new createTransactionController(request);
+            controller.Execute();
+
+            // get the response from the service (errors contained if any)
+            var response = controller.GetApiResponse();
+
+            if (response.messages.resultCode == messageTypeEnum.Ok)
+            {
+                if (response.transactionResponse != null)
+                {
+                    Console.WriteLine("Success, Auth Code : " + response.transactionResponse.authCode);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.messages.message[0].code + "  " + response.messages.message[0].text);
+                if (response.transactionResponse != null)
+                {
+                    Console.WriteLine("Transaction Error : " + response.transactionResponse.errors[0].errorCode + " " + response.transactionResponse.errors[0].errorText);
+                }
+            }
+
+        }
+    }
+}
---
+++ TestAPI_ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using AuthorizeNet.Api.Contracts.V1;
+using System;
+
+namespace TestAPI_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var apiLogin = "3C6M9JggD";
+
+            var tk = "24V9zfbeV6M73b88";
+
+            //ChargeCreditCard.Run("3C6M9JggD", "24V9zfbeV6M73b88");
+
+            Console.WriteLine("Authorize credit card.");
+
+            AuthorizeCreditCard.Run(apiLogin, tk, 200);
+
+            Console.WriteLine("Do a ChasePay Transaction....");
+
+            createTransactionResponse response = CreateChasePayTransaction.Run(apiLogin, tk) as createTransactionResponse;
+
+            var transactionId = response!=null?  response.transactionResponse.transId: string.Empty;
+
+            if (!string.IsNullOrEmpty(transactionId))
+            {
+                Console.WriteLine("Do a Refund Transaction....");
+
+                RefundTransaction.Run(apiLogin, tk, 133.45m, transactionId);
+            }
+
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+        }
+    }
+}
